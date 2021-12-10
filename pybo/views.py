3e902895efdc95e3ecdd657688bab0a3d30874a1
jhs512 <jangka512@gmail.com>
2021-12-10 12:07:11 +0900

    pybo/ URL에서 질문 목록표시

--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse
 
+from pybo.models import Question
+
 
 def index(request):
-    return HttpResponse("안녕!")
+    questions = Question.objects.order_by('-id')
+
+    html = "<ul>\n"
+
+    for q in questions:
+        html += f'<li>{q.id} / {q.create_date} / {q.subject}</li>\n'
+
+    html += "</ul>\n"
+
+    return HttpResponse(html)
