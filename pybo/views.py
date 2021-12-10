a5e7397d4f9dbb96df84aa24254751ebc8ac7128
jhs512 <jangka512@gmail.com>
2021-12-10 12:34:33 +0900

    질문상세 템플릿을 이용해서 출력

--- pybo/views.py
+++ pybo/views.py
@@ -10,3 +10,10 @@ def index(request):
     context = {'question_list' : question_list}
 
     return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    question = Question.objects.get(id = question_id)
+
+    context = {'question' : question}
+
+    return render(request, 'pybo/question_detail.html', context)
